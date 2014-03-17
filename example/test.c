42fc2f92d9a1922bda829e87a05dfe876698f770
huyuzhang <hyzwowtools@gmail.com>
2014-03-17 08:38:46 -0400

    add loop analysis

--- example/test.c
+++ example/test.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int test(int i)
+
+void e()
 {
-	i = i + 1;
-	return i;
+    printf("this is e\n");
+}
+
+void d()
+{
+    printf("this is d\n");
+}
+
+void c()
+{
+    printf("this is c\n");
+}
+
+void b(int i)
+{
+    printf("this is b\n");
+
+    if (i == 1) {
+        c();
+    } else {
+        for (int i=0; i<2; i++) {
+            d();
+        }
+
+        e();
+    }
+}
+
+void a()
+{
+    printf("this is a\n");
+    b(1);
+    b(2);
 }
 
 
 int main(int argc, char** argv)
 {
-	int i = test(1);
-	printf("i = %d\n", i);
-	return 0;
+    for (int i=0; i<=2; i++) {
+        a();
+    }
+    return 0;
 }
--- tools/formatter.py
+++ tools/formatter.py
@@ -6,31 +6,169 @@ import getopt
 import string
 
 trace_file = ""
-prefix = ""
+
+# To understand the basic working flow, let's see an example, if there is a call
+# graph like:
+#a
+#|--b
+#|  |--c
+#|--b
+#|  |--d
+#|  |--d
+#|  |--e
+
+# And, the data sturcture will be like this:
+#{
+#    prefix = "xx",
+#    func = "xx",
+#    func_location = "xx",
+#    caller = "xx",
+#    caller_location = "xx",
+#    times = n,
+#    next = [],
+#}
+#
+# So the full memory data will be:
+#[
+#    {
+#        prefix = "",
+#        func = "a",
+#        func_location = "xx",
+#        caller = "xx",
+#        caller_location = "xx",
+#        times = 1,
+#        next = [
+#            {
+#                prefix = "..",
+#                func = "b",
+#                func_location = "xx",
+#                caller = "xx",
+#                caller_location = "xx",
+#                times = 1,
+#                next = [
+#                    {
+#                        prefix = "....",
+#                        func = "c",
+#                        func_location = "xx",
+#                        caller = "xx",
+#                        caller_location = "xx",
+#                        times = 1,
+#                        next = []
+#                    }
+#                ]
+#            },
+#            {
+#                prefix = "..",
+#                func = "b",
+#                func_location = "xx",
+#                caller = "xx",
+#                caller_location = "xx",
+#                times = 1,
+#                next = [
+#                    {
+#                        prefix = "....",
+#                        func = "d",
+#                        func_location = "xx",
+#                        caller = "xx",
+#                        caller_location = "xx",
+#                        times = 2,
+#                        next = []
+#                    },
+#                    {
+#                        prefix = "....",
+#                        func = "e",
+#                        func_location = "xx",
+#                        caller = "xx",
+#                        caller_location = "xx",
+#                        times = 1,
+#                        next = []
+#                    }
+#                ]
+#            }
+#        ]
+#    }
+#]
 
 def load_trace_file():
-    file = open(trace_file, "r")
+    try:
+        file = open(trace_file, "r")
 
-    for line in file.readlines():
-        value_list = line.strip('\n').split(" ")
+        call_graph = []
+        gen_report(file, "", call_graph)
+        dump_graph(call_graph)
+    finally:
+        file.close()
 
+def create_frame(prefix, func_name, func_location, caller_name, caller_location):
+    return {
+        'prefix' : prefix,
+        'func_name' : func_name,
+        'func_location' : func_location,
+        'caller_name' : caller_name,
+        'caller_location' : caller_location,
+        'times' : 1,
+        'next' : []
+    }
+
+
+def gen_report(file, prefix, call_list):
+    while True:
+        line = file.readline()
+        print line
+        if not line:
+            return
+
+        value_list = line.strip('\n').split(' ')
+        # get the data from one line
         type = value_list[0]
+        # func_name and func_location is the primary key to identify a unique
+        # function
         func_name = value_list[1]
         func_location = value_list[2]
         caller_name = value_list[3]
-        caller_location = value_list[4]
-        gen_report_line(type, func_name, func_location, caller_name, caller_location)
+        caller_location = os.path.basename(value_list[4])
 
-def gen_report_line(type, func_name, func_location, caller_name, caller_location):
-    global prefix
-    if type == "E":
-        prefix += ".."
+        # we entry into next call
+        if type == "E":
+            frame = create_frame(prefix, func_name, func_location, caller_name, caller_location)
 
-        # cut off the prefix of path
-        caller_location = os.path.basename(caller_location)
-        print "%s %s(%s) - (called from %s)" % (prefix, func_name, func_location, caller_location)
-    elif type == "X":
-        prefix = prefix[2:]
+            gen_report(file, frame['prefix'] + "..", frame['next'])
+
+            # if the last frame is equal to the next_call_list, we only need to
+            # increase the times counter in the frame
+            if not call_list:
+                call_list.append(frame)
+                print "first frame: %s" % str(call_list)
+
+            # compare frame with last frame exclude the times field
+            # To compare it, we should set the last_frame.times = 1 first,
+            # then do the comparison
+            else:
+                last_frame = call_list[-1]
+                prev_times = last_frame['times']
+                last_frame['times'] = 1
+
+                if last_frame == frame:
+                    last_frame['times'] = prev_times + 1
+                    print "times + 1, frame: %s" % str(call_list)
+                else:
+                    last_frame['times'] = prev_times
+                    call_list.append(frame)
+                    print "append frame: %s" % str(call_list)
+
+        # we exit from a call
+        elif type == "X":
+            return
+
+def dump_graph(call_graph):
+    for frame in call_graph:
+        print "%s %dx %s(%s) - (called from %s)" % (frame['prefix'],
+                                                    frame['times'],
+                                                    frame['func_name'],
+                                                    frame['func_location'],
+                                                    frame['caller_location'])
+        dump_graph(frame['next'])
+
 
 def usage():
     print "usage: formatter.py -f trace.txt"
